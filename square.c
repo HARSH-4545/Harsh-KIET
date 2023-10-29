6225be7ed381997b029395938ba5b876d344cd85
HARSH-4545 <149269219+HARSH-4545@users.noreply.github.com>
2023-10-30 00:02:27 +0530

    Add files via upload

---
+++ square.c
@@ -0,0 +1,16 @@
+//Calculate Square of a Number -
+
+#include<stdio.h>
+int main ()
+{
+int a;
+
+printf("Enter the value of a :");
+scanf("%d" ,&a);
+
+printf("The square of a is :%d",a*a);
+
+return 0;
+
+}
+
